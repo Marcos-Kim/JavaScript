ebd62b12cac81c949408e17af50efa6575bad8e2
Marcos-Kim <marcos.skim@senacsp.edu.br>
2024-03-14 17:27:20 -0300

    botões, formatação e js externo

---
+++ js/acoes.js
@@ -0,0 +1,4 @@
+//alert('funcionou!');
+'use strict'
+
+document.getElementById('caixa').innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum tristique nisl eu mollis. Donec placerat rhoncus posuere. Ut finibus purus ut metus placerat, sit amet eleifend lorem elementum. Fusce.'
---
+++ js/coresesignificado.js
@@ -0,0 +1,1 @@
+button('azul').style.backgroundColor = 'blue'
