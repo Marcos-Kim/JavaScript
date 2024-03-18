7827c56462ddd8b1e9eaa7a8467083399f61b110
Marcos-Kim <marcos.skim@senacsp.edu.br>
2024-03-18 17:24:58 -0300

    funções em js

--- js/acoes.js
+++ js/acoes.js
@@ -1,4 +1,45 @@
-//alert('funcionou!');
 'use strict'
+    
+    var btazul = document.getElementById('btazul');
+    var btverde = document.getElementById('btverde');
+    var btlaranja = document.getElementById('btlaranja');
+    var textoH2 = document.querySelector('#textos');
+    //alert("Funcionou!");
 
-document.getElementById('caixa').innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum tristique nisl eu mollis. Donec placerat rhoncus posuere. Ut finibus purus ut metus placerat, sit amet eleifend lorem elementum. Fusce.'
+// btazul.onmouseover = () => document.body.style.backgroundColor = "blue"; 
+// btazul.onclick = () => textoH2.textContent = 'Azul Significa Harmonia';
+// btverde.onmouseover = () => document.body.style.backgroundColor = "green"; 
+// btverde.onclick = () => textoH2.textContent = 'Verde Significa Saúde e Esperança';
+// btlaranja.onmouseover = () => document.body.style.backgroundColor = "orange";
+// btlaranja.onclick = () => textoH2.textContent = 'Laranja Significa Força e Energia';
+
+
+    
+    
+
+btazul.addEventListener('click' , function(){
+       // document.className = '' //Limpar Classe (versao mais simples)
+        document.body.classList.remove('btverde' , 'btlaranja');
+        document.body.classList.add('btazul');
+        textoH2.textContent = 'Azul Significa Harmonia e Maturidade';
+        
+    });
+    
+    
+    
+    btverde.addEventListener('click' , function(){
+       // document.className = '' //Limpar Classe (versao mais simples)
+        document.body.classList.remove('btazul' , 'btlaranja');
+        document.body.classList.add('btverde');
+        textoH2.textContent =  'Verde Significa Saude e Esperanca';
+        
+    });
+
+    btlaranja.addEventListener('click' , function(){
+        //document.className = '' //Limpar Classe (versao mais simples)
+        document.body.classList.remove('btverde' , 'btazul');
+        document.body.classList.add('btlaranja');
+       textoH2.textContent = 'Laranja Significa Forca e Energia';
+              
+        
+    });
