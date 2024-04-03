8800f2cf03f3e1bf87949eac1cfdd0b013e22b2f
Marcos-Kim <marcos.skim@senacsp.edu.br>
2024-04-03 17:16:58 -0300

    modal e contador

--- cadastroFor/assets/js/aula07.js
+++ cadastroFor/assets/js/aula07.js
@@ -1,5 +1,5 @@
 let vetor = []
-for (let posicao = 0;posicao<vetor.length; posicao++){
+for (let posicao = 0; posicao < vetor.length; posicao++) {
     console.log(vetor[posicao])
 }
 
@@ -11,16 +11,43 @@ let telefone = [];
 let assunto = [];
 let msg = [];
 
-formContato.addEventListener("submit", (event) =>{
+formContato.addEventListener("submit", (event) => {
     event.preventDefault();
     nome.push(formContato.nome.value);
     email.push(formContato.email.value);
     telefone.push(formContato.telefone.value);
     assunto.push(formContato.assunto.value);
-    msg.push(formContato.msg.value);
+    msg.push(formContato.mensagem.value);
     gerarLista()
 })
 
-function gerarLista(){
-    
+function gerarLista() {
+    let auxLista = "";
+
+    for (let posicao = 0; posicao < nome.length; posicao++) {
+        auxLista += `<div class="card">
+                        <div class="dados-label">
+                            <div>Nome completo:</div>
+                            <div>${nome[posicao]}</div>
+                        </div>
+                        <div class="dados-label">
+                            <div>Email:</div>
+                            <div>${email[posicao]}</div>
+                        </div>
+                        <div class="dados-label">
+                            <div>Telefone:</div>
+                            <div>${telefone[posicao]}</div>
+                        </div>
+                        <div class="dados-label">
+                            <div>Assunto:</div>
+                            <div>${assunto[posicao]}</div>
+                        </div>
+                        <div class="dados-label">
+                            <div>Mensagem:</div>
+                            <div>${msg[posicao]}</div>
+                        </div>
+                    </div>`;
+    }
+    divLista.innerHTML = auxLista;
+
 }
